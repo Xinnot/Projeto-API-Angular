bea6cc427ed7e0e104bee9278a589e79cc29bf1f
Everton Andrade <63211896+Xinnot@users.noreply.github.com>
2023-03-02 15:13:25 -0300

    Add files via upload
    Projeto finalizado.

---
+++ src/app/guard.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
+  constructor(private _router: Router) { }
+  canActivate(
+    _route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (localStorage.getItem('token') !== null) {
+      return true;
+    }
+    this._router.navigate(['/login'])
+    return false;
+  }
+}
